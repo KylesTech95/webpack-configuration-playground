5e8cd253184156ca1436dfb40cf7bf49e603c967
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-06-21 19:13:04 -0400

    feat: browserlist config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const MiniCssExpressPlugin = require('mini-css-extract-plugin')
+const MiniCssExpressPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    plugins:[new MiniCssExpressPlugin()],
+  plugins: [new MiniCssExpressPlugin()],
   mode: "development",
   module: {
     rules: [
@@ -8,9 +8,14 @@ module.exports = {
         // test for loaders: css, scss
         test: /\.(s)?css$/i,
         exclude: /\/node_modules/i,
-        use: [MiniCssExpressPlugin.loader, "css-loader", "sass-loader","postcss-loader"], // how do you want me to handle this?
+        use: [
+          MiniCssExpressPlugin.loader,
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
+        ], // how do you want me to handle this?
       },
     ],
   },
-  devtool:"source-map"
+  devtool: "source-map",
 };
